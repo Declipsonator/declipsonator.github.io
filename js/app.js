634fad3bb88670c1a234987fa4718599833ecb9e
jackson <declipsonator@gmail.com>
2024-07-03 10:04:02 -0400

    More marquee stuff and about

--- js/app.js
+++ js/app.js
@@ -14,3 +14,36 @@ function randomIntFromInterval(min, max) { // min and max included
 
 swapInitials();
 
+document.addEventListener('DOMContentLoaded', function() {
+  const marquees = document.querySelectorAll('.marquee');
+  const screenWidth = window.innerWidth;
+
+  const marqueeInsides = document.querySelectorAll('.marqueeInside');
+  for (let marqueeInside of marqueeInsides) {
+    marqueeInside.style.setProperty('--rand', randomIntFromInterval(5, 20) + 's');
+  }
+
+
+  for(let marquee of marquees) {
+    if (randomIntFromInterval(0, 1) === 0) {
+      marquee.className = 'marqueeOpposite';
+    }
+
+
+
+    let additionalContent = '';
+    const repetitions = Math.floor(screenWidth / 1000);
+    for (let i = 0; i < repetitions + 1; i++) {
+      additionalContent += marquee.innerHTML;
+    }
+    marquee.innerHTML += additionalContent;
+
+
+
+  }
+});
+
+
+document.getElementById('aboutButton').addEventListener('click', function() {
+  document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+});
